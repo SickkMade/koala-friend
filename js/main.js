c970228164f58b3be218dac3d02ff6e37d924b08
sickk <jaakmyr@gmail.com>
2024-08-18 00:54:58 -0700

    added a bunch of little guys

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-const koala = document.querySelector('#koala')
+const koalafirst = document.querySelector('.koala')
 
-koala.addEventListener('mouseover', () => {
+for(let i = 0; i < 20; i++){
+    let temp = koalafirst.cloneNode(true)
+    temp.setAttribute('data-speed', Math.max(1,Math.random()*3))
+    temp.style.top = Math.random()*window.innerHeight+'px';
+    document.body.appendChild(temp)
+}
+
+const koalaz = document.querySelectorAll('.koala')
+
+koalaz.forEach(koala => koala.addEventListener('mouseover', () => {
     alert('YOU LOSE! you must look at my website now')
     window.location.replace("https://sickkmade.github.io")
-})
+}))
 
 let mouseX
 let mouseY
@@ -13,19 +22,24 @@ document.addEventListener('mousemove', event => {
     mouseY = event.clientY
 });
 
-function moveKoala(){
+function moveKoala(koala){
     if(!(mouseX && mouseY)) return
 
     let differenceX = koala.offsetLeft - mouseX
     let differenceY = koala.offsetTop - mouseY
+    let speed = koala.getAttribute('data-speed')
     let distance = Math.sqrt(differenceX*differenceX + differenceY*differenceY)
 
     let deg = Math.atan2(mouseY - koala.offsetTop, mouseX - koala.offsetLeft)
 
-    koala.style.left = koala.offsetLeft - (differenceX/distance)+'px'
-    koala.style.top =  koala.offsetTop - (differenceY/distance)+'px'
+    koala.style.left = koala.offsetLeft - (differenceX/distance)*speed+'px'
+    koala.style.top =  koala.offsetTop - (differenceY/distance)*speed+'px'
 
     koala.style.transform = `translate(-50%,-50%) rotate(${deg}rad)`
 }
 
-setInterval(() => requestAnimationFrame(moveKoala), 1)
+function moveKoalas(){
+    koalaz.forEach((koala) => moveKoala(koala))
+}
+
+setInterval(() => requestAnimationFrame(moveKoalas), 1)
